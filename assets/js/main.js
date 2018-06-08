d4afc55cc83e539df7ee0193c134d91d6f789630
DiegoFRM <diego@yogome.com>
2018-06-08 11:07:12 -0500

    update questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*DIEGO ROJAS 2018*/
-alert("Actualización 7 de Junio del 2018");
+alert("Actualización 8 de Junio del 2018");
 selectQuestion = 0;
 //SOUNDS*****************
 var soundActive = true;
